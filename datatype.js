44b11d580d91f87d218a4444d10eb53849b17710
Abunsrr <110284949+Abunsrr@users.noreply.github.com>
2023-08-16 12:21:20 +0530

    while ex1

--- datatype.js
+++ datatype.js
@@ -2,3 +2,12 @@
 // alert("hello")  we are using node js not browser
 console.log(3+3) 
 console.log("hitesh");
+
+//......premittive data type......//
+// number => 2 to power53
+//bigint for big number
+//string=""
+//boolean=>true false
+//null=stand alone value i.e empty
+//undefine:which is not define yet
+//symbol=unique 
