f2b76e00e98a9e62c99fb91b472a5bfae896d244
Abunsrr <110284949+Abunsrr@users.noreply.github.com>
2024-03-29 20:10:24 +0530

    new  upadate

--- dummy.js
+++ dummy.js
@@ -1,5 +1,16 @@
+// for (let i = 1; i <= 10; i++) {
+//    console.log(`outer loop is ${i}`);
+//    for (let j = 1; j <= 10; j++) {
+//       console.log(`${i} * ${j} = ${i*j}`);
+//    }
    
+// }/
 
-
-   console.log(num)
-   let num =5;
+let i=6
+if(i=6){
+   console.log(`outer loop is ${i}`);
+   for (let j = 1; j <= 10; j++) {
+      console.log(`${i} * ${j} = ${i*j}`);
+   }
+   
+}
