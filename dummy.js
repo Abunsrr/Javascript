4a264829d3ca168a3d5a1ffb71d8511f4eecd5d5
Abunsrr <110284949+Abunsrr@users.noreply.github.com>
2024-04-03 17:21:09 +0530

    update

--- dummy.js
+++ dummy.js
@@ -1,51 +1,16 @@
-const myNums=[1,2,3]
+function myfilter(arr, callback) {
+    const filterarray=[]
 
-// const mytotal= myNums.reduce(function(acc , cur ) {
-   
-//    console.log(`acc: ${acc} and cur: ${cur}`)
-//    return acc + cur},0)
-// console.log(mytotal);
+    for (let i = 0; i < arr.length; i++) {
+        if(callback(arr[i],i,arr)){
+            filterarray.push(arr[i])
+        }
 
-// const mytotal= myNums.reduce( 
-//    (acc ,cur) => acc+cur,0
-// )
-// console.log(mytotal);
-
-
-// let val='0123456789ABCDEF'
-// let cons= '#'
-// let arrayval =[...val]
-// for (let i = 0; i < 6; i++) {
-//     console.log(i);
-    
-// }
-
-
-
-// let val = '0123456789ABCDEF';
-// let cons = '#';
-// let generatedHex = cons;
-// for (let i = 0; i < 6; i++) {
- 
-//    let randomIndex= Math.floor(Math.random()*val.length)
-    
-//    generatedHex = generatedHex+ val[randomIndex];
-
-// }
-
-
-
-const randomColor= ()=>{
-    let val='0123456789ABCDEF'
-    let cons= '#'
-    for (let i = 0; i < 6; i++) {
-     
-       let randomIndex= Math.floor(Math.random()*val.length)
         
-       cons = cons+ val[randomIndex];
-    
     }
+   return filterarray   
+}
 
-    return cons;
- }
-console.log(randomColor());
+const num=[1,3,4,6,8,7,9,10]
+const even=myfilter(num, (n)=> n%2===0)
+console.log(even);
