a15de0d90245b3fd14926156777728b7197278b1
Abunsrr <mod.nsrr@gmail.com>
2024-10-17 01:07:56 +0530

    added new code

---
+++ part1/variable/var.js
@@ -0,0 +1,15 @@
+var RandomGuy="nasar befhv"
+let animal= "Lion"
+console.log(window.RandomGuy , window.animal);
+
+
+ function  show(){
+    if(1){
+    var functionScope = "function scope";
+    let localope = "local scope let and const"
+ }
+ console.log(functionScope)
+}
+
+show()
+
